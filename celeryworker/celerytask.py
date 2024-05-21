3e819757285c647b1dccf3d6b5926fbfbdc8591c
Asadullah <asadullah.dev911@gmail.com>
2024-05-21 06:37:42 +0500

    Task routing done

--- celeryworker/celerytask.py
+++ celeryworker/celerytask.py
@@ -1,9 +1,7 @@
 from celery import Celery
+from newapp.tasks import task2
 
 app = Celery("task")
 app.config_from_object("celeryconfig")
-
-
-@app.task
-def add_numbers():
-    return
+app.conf.imports = "newapp.tasks"
+app.autodiscover_tasks()

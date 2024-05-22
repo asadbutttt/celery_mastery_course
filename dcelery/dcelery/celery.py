99b9becc26d56a891463634e08f85ba98418069a
Asadullah <asadullah.dev911@gmail.com>
2024-05-22 06:00:34 +0500

    Task priority in celery with rabbitmq

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -1,9 +1,45 @@
 import os
+import time
 from celery import Celery
+from kombu import Queue, Exchange
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 app = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.task_queues = [
+    Queue(
+        "tasks",
+        Exchange("tasks"),
+        routing_key="tasks",
+        queue_arguements={"x-max-priority": 10},
+    ),
+]
+
+app.conf.task_acks_late = True
+app.conf.task_default_priority = 5
+app.conf.worker_prefetch_multiplier = 1
+app.conf.worker_concurrency = 1
+
+
+@app.task(queue="tasks")
+def t1():
+    time.sleep(5)
+    return
+
+
+@app.task(queue="tasks")
+def t2():
+    time.sleep(5)
+    return
+
+
+@app.task(queue="tasks")
+def t3():
+    time.sleep(5)
+    return
+
+
 # app.conf.task_routes = {
 #     "newapp.tasks.task1": {"queue": "queue1"},
 #     "newapp.tasks.task2": {"queue": "queue2"},

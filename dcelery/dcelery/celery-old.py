0c79dd4732070b4207ec6d915c75117b32aad5c4
Asadullah <asadullah.dev911@gmail.com>
2024-05-22 07:11:39 +0500

    Dynamic autodiscovery of tasks

--- dcelery/dcelery/celery-old.py
+++ dcelery/dcelery/celery-old.py
@@ -1,27 +1,3 @@
-import os
-import time
-from celery import Celery
-from kombu import Queue, Exchange
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
-app = Celery("dcelery")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-
-app.conf.task_queues = [
-    Queue(
-        "tasks",
-        Exchange("tasks"),
-        routing_key="tasks",
-        queue_arguements={"x-max-priority": 10},
-    ),
-]
-
-app.conf.task_acks_late = True
-app.conf.task_default_priority = 5
-app.conf.worker_prefetch_multiplier = 1
-app.conf.worker_concurrency = 1
-
-
 @app.task(queue="tasks")
 def t1(a, b, message=None):
     result = a + b

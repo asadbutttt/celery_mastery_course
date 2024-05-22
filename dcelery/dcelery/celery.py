224c27253e3c063092e537aac4f058f564822eac
Asadullah <asadullah.dev911@gmail.com>
2024-05-22 05:12:46 +0500

    Rabbitmq setup

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -9,12 +9,12 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 #     "newapp.tasks.task2": {"queue": "queue2"},
 # }
 
-app.cnof.task_default_rate_limit = "1/m"
+# app.cnof.task_default_rate_limit = "1/m"
 
-app.conf.broker_transport_options = {
-    "priority_steps": list(range(10)),
-    "sep": ":",
-    "queue_order_strategy": "priority",
-}
+# app.conf.broker_transport_options = {
+#     "priority_steps": list(range(10)),
+#     "sep": ":",
+#     "queue_order_strategy": "priority",
+# }
 
 app.autodiscover_tasks()

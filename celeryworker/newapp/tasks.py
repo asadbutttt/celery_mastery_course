fb3c3288896865ec6d86358e751c02fedfdc0da7
Asadullah <asadullah.dev911@gmail.com>
2024-05-27 08:33:06 +0500

    Check webpage celery worker added

--- celeryworker/newapp/tasks.py
+++ celeryworker/newapp/tasks.py
@@ -3,6 +3,20 @@ import time
 from celery import group, shared_task
 from django.core.management import call_command
 
+import requests
+from celery import shared_task
+from sentry_sdk import capture_exception
+
+
+@shared_task
+def check_webpage():
+    try:
+        response = requests.get("http://127.0.0.1:8001")
+        if response.status_code != 200:
+            raise Exception(f"Website is down...lets panic!")
+    except requests.exceptions.RequestException as e:
+        capture_exception(e)
+
 
 @shared_task
 def management_command():

c936ec0ee0b03749ad34a19ef90ef62d9df63473
Asadullah <asadullah.dev911@gmail.com>
2024-05-27 08:21:16 +0500

    Test commands

--- celeryworker/newapp/tasks.py
+++ celeryworker/newapp/tasks.py
@@ -1,11 +1,30 @@
-from celery import shared_task
+import time
+
+from celery import group, shared_task
+from django.core.management import call_command
 
 
 @shared_task
-def task1():
-    return
+def management_command():
+    call_command("test_command")
 
 
-@shared_task
-def task2():
-    return
+# @shared_task
+# def tp1(queue='celery'):
+#     time.sleep(3)
+#     return
+
+# @shared_task
+# def tp2(queue='celery:1'):
+#     time.sleep(3)
+#     return
+
+# @shared_task
+# def tp3(queue='celery:2'):
+#     time.sleep(3)
+#     return
+
+# @shared_task
+# def tp4(queue='celery:3'):
+#     time.sleep(3)
+#     return
